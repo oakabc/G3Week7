e20de2c1b2e343fc82fd5b550d28bb9d111b51c5
chanakarnkingkaew <chanakarnkingkaew@localhost>
2022-07-20 12:51:05 +0700

    First commit of Week 7

---
+++ src/main/java/Sample.java
@@ -0,0 +1,13 @@
+class Demo{ 
+public static void demoMethod() {
+ System.out.println("method of super class"); 
+} 
+} 
+public class Sample extends Demo { 
+public static void demoMethod() { 
+System.out.println("method of sub class"); 
+} 
+public static void main(String args[] ) { 
+Sample.demoMethod(); 
+}
+}
---
+++ src/main/java/com/mycompany/g3week7/Shape.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.g3week7;
+
+/**
+ *
+ * @author chanakarnkingkaew
+ */
+public class Shape {
+    
+    // Overload 
+    public Shape () {
+    
+    }
+    
+    public Shape (int x) {
+    
+    
+    }
+    
+    public Shape (int x, int y) {
+    
+    }
+    
+    public Shape (double x) {
+    
+    
+    }
+    
+
+    @Override
+    public String toString () { // Object Class
+        
+        
+    
+        return null;
+    }
+
+   
+    @Override
+    public boolean equals (Object object) {
+    
+        return true;
+    }
+    
+}
